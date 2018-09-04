54251db80a68e1933143a49a4866f4d5f2d9abf9
JonZarate <jon.zarate@hotmail.com>
2018-09-04 12:16:00 +0100

    Created project, set theming and added some libraries

---
+++ app/src/main/java/com/jonzarate/fanduelgame/view/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.jonzarate.fanduelgame.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.jonzarate.fanduelgame.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ readme.md
@@ -0,0 +1,2 @@
+# FanDuel Game
+
